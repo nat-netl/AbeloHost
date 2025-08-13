c083461e347e7a8e917ea01357b466cdc841123c
Danil <nat_netl3@bk.ru>
2025-08-13 14:32:20 +0400

    Верстка авторизации

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,5 +1,22 @@
+import { DeffaultButton } from "@/ui-components/buttons/DeffaultButton";
+import { Input } from "@/ui-components/input";
+import classNames from "classnames";
+
+import styles from "./styles.module.scss";
+
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <div className={styles.box}>
+      <div className={classNames("container", styles.wrapper)}>
+        <h1 className={styles.title}>Login</h1>
+        <form className={styles.form}>
+          <Input className={styles.input} placeholder="Username" error="" />
+          <Input className={styles.input} placeholder="Password" type="password" error="" />
+          <DeffaultButton className={styles.btn}>Login</DeffaultButton>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
