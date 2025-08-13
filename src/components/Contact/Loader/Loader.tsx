5a6786fa3b807834ac7636ae9410efad8274e0ab
Danil <nat_netl3@bk.ru>
2025-08-13 23:33:53 +0400

    Добавил компонент loder

---
+++ src/components/Contact/Loader/Loader.tsx
@@ -0,0 +1,7 @@
+import styles from "./style.module.scss";
+
+const Loader = () => {
+  return <div className={styles.loader}></div>;
+};
+
+export default Loader;
--- src/components/Products/Products.tsx
+++ src/components/Products/Products.tsx
@@ -3,21 +3,29 @@
 import { useProducts } from "@/hooks/getProducts";
 import classNames from "classnames";
 
+import { Loader } from "../Contact/Loader";
 import { ProductCard } from "../ProductCard";
 import styles from "./styles.module.scss";
 
 const Products = () => {
-  const { products } = useProducts();
+  const { products, loading } = useProducts();
 
   return (
     <div className={styles.box}>
       <div className={classNames("container", styles.wrapper)}>
         <h1 className={styles.title}>Products</h1>
-        <div className={styles.products}>
-          {products.map((product) => {
-            return <ProductCard key={product.id} {...product} />;
-          })}
-        </div>
+
+        {loading ? (
+          <div className={styles.loader}>
+            <Loader />
+          </div>
+        ) : (
+          <div className={styles.products}>
+            {products.map((product) => {
+              return <ProductCard key={product.id} {...product} />;
+            })}
+          </div>
+        )}
       </div>
     </div>
   );
