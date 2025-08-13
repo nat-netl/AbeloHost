62e4d6deacafe6104233b1c5dcefe5243bd43cc5
Danil <nat_netl3@bk.ru>
2025-08-13 13:39:10 +0400

    Компонент login

---
+++ src/app/login/page.tsx
@@ -0,0 +1,13 @@
+import { Header } from "@/components/Header";
+import { Login } from "@/components/Login";
+
+const page = () => {
+  return (
+    <>
+      <Header />
+      <Login />
+    </>
+  );
+};
+
+export default page;
