057158e7652aa8b4f27d4a1edeb3fdaa7d31b2e5
Danil <nat_netl3@bk.ru>
2025-08-13 17:07:31 +0400

    Вывел пользователя в footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,19 +1,20 @@
-import { LinkOverlay } from "@/ui-components/links/Link";
+"use client";
+
+import useAuthStore from "@/store/authStore";
 import classNames from "classnames";
 
-import { Contacts } from "../Contacts";
 import styles from "./styles.module.scss";
 
 const Footer = () => {
+  const { user } = useAuthStore();
+  const currentYear = new Date().getFullYear();
   return (
     <footer className={styles.box}>
-      <Contacts />
-
       <div className={classNames("container", styles.wrapper)}>
-        <LinkOverlay href="/" className={styles.logo}>
-          <span>AbeloHost Shop</span>
-          <span className={styles.dot}>.</span>
-        </LinkOverlay>
+        <div className={styles.info}>
+          <span>{currentYear}</span>
+          {user ? <span> `Logged as ${user.email}` </span> : ""}
+        </div>
       </div>
     </footer>
   );
